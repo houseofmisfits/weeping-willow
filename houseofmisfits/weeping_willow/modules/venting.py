9f880ba197db93cd29c8ca3c5066e35beb986395
Paco Rainwater <paco@heckin.io>
2019-12-03 11:31:12 -0600

    Scan for undeleted messages every 10 minutes

--- houseofmisfits/weeping_willow/modules/venting.py
+++ houseofmisfits/weeping_willow/modules/venting.py
@@ -6,6 +6,8 @@ import logging
 
 from asyncio import sleep
 
+from discord import TextChannel
+
 from houseofmisfits.weeping_willow.modules import Module
 from houseofmisfits.weeping_willow.triggers import ChannelTrigger
 
@@ -22,6 +24,7 @@ class VentingModule(Module):
         self.deletion_schedules = {}
         self.messages = {}
         self.is_open = True
+        self.scan_time = datetime.now()
         self.client.loop.create_task(self.run_loop())
 
     def get_triggers(self):
@@ -38,6 +41,9 @@ class VentingModule(Module):
         while self.is_open:
             await sleep(1)
             await self.execute_scheduled_deletions()
+            if self.scan_time <= datetime.now():
+                await self.scan_messages()
+                self.scan_time = datetime.now() + timedelta(minutes=10)
 
     async def execute_scheduled_deletions(self):
         for message_id in self.deletion_schedules.copy():
@@ -52,4 +58,13 @@ class VentingModule(Module):
                     del self.deletion_schedules[message_id]
                     del self.messages[message_id]
 
+    async def scan_messages(self):
+        logger.debug("Scanning for missed messages")
+        channel: TextChannel = await self.client.fetch_channel(self.config['channel_id'])
+        async for message in channel.history(limit=200):
+            if message.id not in self.deletion_schedules:
+                logger.debug("Found message {} not scheduled for deletion, adding to queue".format(
+                    message.id
+                ))
+                self.process(message)
 
