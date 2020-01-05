4a8c7a4eea23f2295bf7257dd5c5c4ee4a6295b4
Paco Rainwater <paco@heckin.io>
2020-01-05 13:57:40 -0600

    Refactor and document WeepingWillowClient

--- houseofmisfits/weeping_willow/client.py
+++ houseofmisfits/weeping_willow/client.py
@@ -26,43 +26,6 @@ class WeepingWillowClient(discord.Client):
         self.modules = []
         self.triggers: List[Trigger] = []
 
-    async def set_up_modules(self):
-        logger.debug("Setting up modules")
-        # Remember that trigger processing is first-come, first-serve! If there is any kind of conflict, the first
-        # module registered takes precedence.
-        for module_name in modules.__module_list__:
-            module_class: ClassVar[modules.Module] = getattr(modules, module_name)
-            module = module_class(self)
-            self.modules.append(module)
-            logger.debug("Added module: {}".format(module_class.__name__))
-            async for trigger in module.get_triggers():
-                if trigger is not None:
-                    self.add_trigger(trigger)
-
-    def add_trigger(self, trigger: Trigger):
-        logger.debug("Adding trigger {}".format(str(trigger)))
-        self.triggers.append(trigger)
-
-    async def on_ready(self):
-        await self.set_up_modules()
-
-    async def on_message(self, message: discord.Message):
-        for trigger in self.triggers:
-            triggered_fn = await trigger.evaluate(message)
-            if triggered_fn:
-                # noinspection PyUnresolvedReferences
-                logger.debug(
-                    "Message ID {0.id} ({0.author.display_name} in {0.channel.id}) triggered module {1}".format(
-                        message, triggered_fn.__module__
-                    )
-                )
-                if await triggered_fn(message):
-                    return
-                # noinspection PyUnresolvedReferences
-                logger.debug("Message {0.id}: {1} did not report successful processing. Continuing processing.".format(
-                    message, triggered_fn.__module__
-                ))
-
     def run(self, *args, **kwargs):
         logger.info("Bot is starting, use {} to invite bot to server".format(
             discord.utils.oauth_url(
@@ -79,7 +42,57 @@ class WeepingWillowClient(discord.Client):
         await self.data_connection.close()
         await super(WeepingWillowClient, self).close()
 
+    async def on_ready(self):
+        """
+        Runs when the bot is connected to Discord and ready to do stuff
+        """
+        await self.set_up_modules()
+
+    async def set_up_modules(self):
+        """
+        Gets all of the modules in the modules package and sets them up
+        """
+        logger.debug("Setting up modules")
+        # Remember that trigger processing is first-come, first-serve! If there is any kind of conflict, the first
+        # module registered takes precedence.
+        for module_name in modules.__module_list__:
+            module_class: ClassVar[modules.Module] = getattr(modules, module_name)
+            module = module_class(self)
+            self.modules.append(module)
+            logger.debug("Added module: {}".format(module_class.__name__))
+            async for trigger in module.get_triggers():
+                if trigger is not None:
+                    self.add_trigger(trigger)
+
+    def add_trigger(self, trigger: Trigger):
+        logger.debug("Adding trigger {}".format(str(trigger)))
+        self.triggers.append(trigger)
+
+    async def on_message(self, message: discord.Message):
+        """
+        When a message occurs, this cycles through all of the triggers that have been set up and checks if any of them
+        match.
+        """
+        for trigger in self.triggers:
+            triggered_fn = await trigger.evaluate(message)
+            if triggered_fn:
+                # noinspection PyUnresolvedReferences
+                logger.debug(
+                    "Message ID {0.id} ({0.author.display_name} in {0.channel.id}) triggered module {1}".format(
+                        message, triggered_fn.__module__
+                    )
+                )
+                if await triggered_fn(message):
+                    return
+                # noinspection PyUnresolvedReferences
+                logger.debug("Message {0.id}: {1} did not report successful processing. Continuing processing.".format(
+                    message, triggered_fn.__module__
+                ))
+
     async def get_admin_users(self):
+        """
+        Returns all of the users in the designated roles who can perform administration commands, etc.
+        """
         guild_id = os.getenv('BOT_GUILD_ID')
         if guild_id is None:
             logger.warning("Cannot get admin users - Guild ID not set")
@@ -94,5 +107,4 @@ class WeepingWillowClient(discord.Client):
         for role in guild.roles:
             if role.id in [tech_role, admin_role]:
                 admin_users += role.members
-        logger.debug("Admin members:\n" + "\n".join(str(member.id) for member in admin_users))
         return admin_users
